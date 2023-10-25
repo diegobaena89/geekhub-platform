cd5bb0010a6598eb25e8200f3a34e77fa77b838c
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-10-25 18:07:38 -0300

    feat: Main page component

---
+++ frontend/src/Pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import Header from "../../components/Header";
+import background from "../../assets/background.svg"
+
+function Home() {
+    return (
+        <section
+            className="h-screen relative mx-auto max-w-screen-lg px-4 md:px-0 bg-contain bg-no-repeat bg-right z-10"
+            style={{
+                backgroundImage: `url(${background})`
+            }}>
+            <Header />
+            <article
+            className="text-left h-200 ml-11 absolute"
+            style={{ bottom: '10rem' }}
+            >
+                <h1 className="text-custom-gray max-w-md overflow-x-hidden"
+                  style={{ fontSize: '1.1rem'}}
+                >
+                    Somos uma plataforma<br />
+                    que conecta colecionadores<br />
+                    em busca de outros colecionadores.
+                </h1>
+                <div className="flex ml-10 mt-8">
+                    <button className="text-white bg-custom-green px-6 py-1 rounded-xl mr-4">
+                        SignUp
+                    </button>
+                    <button className="text-custom-green bg-white px-6 py-1 rounded-xl mr-4 border border-custom-green">
+                        SignUp
+                    </button>
+                </div>
+            </article>
+        </section>
+    )
+}
+
+export default Home;
