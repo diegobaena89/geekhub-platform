767d5e3f9c0f45362daafdbd3856bae22b7f9bb2
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-10-25 18:11:36 -0300

    creating user with post

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'; 
 
 @Injectable()
 export class UsersService {
+  private readonly users: any = {
+    id: 1,
+    name: "Diego",
+    email: "diegodograu@gmail.com"
+  }
   getAllUsers(): any[] {
     return [
       { id: 1, name: 'John Doe', email: 'jhondoe@gmail.com' },
@@ -12,4 +17,15 @@ export class UsersService {
   getUserById(id: number): any {
     return { id: 1, name: 'John Doe', email: 'jhondoe@gmail.com' };
   }
+
+  createUser(user: any[]) {
+    const newUser = {
+      ...user,
+    }
+    this.users.push(newUser)
+    
+    return newUser;
+  }
+
 }
+
