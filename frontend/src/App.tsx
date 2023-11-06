7b7fdbdbd5daf5a92f65a38768c90a649f0ef143
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-11-06 20:25:10 -0300

    feat: create routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './App.css';
 import Home from './Pages/Home';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      {/* <Login />  */}
     </div>
   );
 }
